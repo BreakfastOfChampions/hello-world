cc432f97024f5ccfcc0c17ed363b150ccf050bda
William Snodgrass <william.snodgrass@verscend.com>
2019-03-26 13:54:53 -0400

    Update lambda_function.py
    Updated with new code

--- lambda_function.py
+++ lambda_function.py
@@ -1,14 +1,19 @@
 import tabula
+import pandas
 
+# define class
 class PdfReader:
     def __init__(self, filename):
         self.filename = filename
         self.filetype = filename.rpartition('.')[-1]
-        self.pdf_reader()
+        self.reader()
 
-    def pdf_reader(self):
+    def reader(self):
         if self.filetype == 'pdf':
             self.result = tabula.read_pdf(self.filename, pages = 'all')
+        if self.filetype == 'csv'
+            self.result = pandas.read_csv(self.filename)
+    
 
 def lambda_handler(event):
     # Get data
@@ -16,5 +21,6 @@ def lambda_handler(event):
 
     print(r1.result)
 
+#test file on my local - PDF
 lambda_handler('C:/Users/william.snodgrass/Documents/Provider_Sanction/StateList/IdahoMedicaidExclusionList.pdf')
 
